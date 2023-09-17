64e360338f110deda28ebe1ffc42055dd124502f
mar <purple.giraffey@gmail.com>
2023-09-17 13:31:35 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './assets/lunar-tear-loading.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img  src={logo} alt="lunar-tear" className="App-logo"/>
+        <p>
+          Welcome!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
