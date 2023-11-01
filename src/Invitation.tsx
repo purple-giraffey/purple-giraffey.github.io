2a16b100762cbe5794f3a8134e3923e4b8561778
mar <purple.giraffey@gmail.com>
2023-11-01 19:14:38 +0100

    add kafana location and menu

--- src/Invitation.tsx
+++ src/Invitation.tsx
@@ -7,11 +7,15 @@ import { ReactComponent as PlayIcon } from './assets/play.svg'
 
 import { Guest } from './guests';
 import Menu from './Menu';
+import { MENU_TYPE } from './Menu'
 
-const SHOW_MENU = false
+const SHOW_MENU_ZHURKA = false
+const SHOW_MENU_KAFANA = false
+
+
 
 function Invitation(props: { guest: Guest, audio: any }) {
-    const [isMenuView, setMenuView] = useState(false);
+    const [isMenuView, setMenuView] = useState<MENU_TYPE | null>(null);
     const [isAudioOn, setAudio] = useState(false)
 
     function toggleAudio() {
@@ -28,45 +32,48 @@ function Invitation(props: { guest: Guest, audio: any }) {
         toggleAudio()
     }, []);
 
-    function exitMenu() { setMenuView(false) }
+    function exitMenu() { setMenuView(null) }
 
     const addToCalendarLink = "https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NnBoNmFkaGw2NWo2NmI5bGNoajZhYjlrYzRzajRiOW82MWlqOGI5aDZwZ21jY3BqNjVpbWFjcjZjNCA2YjE2NzAyZmJiZWM2MGVlMTdiYjU2NDc2MWI2Nzc0OWNkODI2YWQ1MGEyOTkyZjBiNTk0OTc4MGZmNzg4ZTA3QGc&tmsrc=6b16702fbbec60ee17bb564761b67749cd826ad50a2992f0b5949780ff788e07%40group.calendar.google.com"
 
     return (
-        isMenuView && SHOW_MENU ? <Menu goBack={() => exitMenu()} /> :
-            <div className="Invitation">
-                {isAudioOn ?
-                    <button className='mute-btn' onClick={() => toggleAudio()}><MuteIcon /></button>
-                    : <button className='mute-btn' onClick={() => toggleAudio()}><PlayIcon /></button>}
+        isMenuView == MENU_TYPE.ZHURKA && SHOW_MENU_ZHURKA ? <Menu goBack={() => exitMenu()} menu={MENU_TYPE.ZHURKA} /> :
+            isMenuView == MENU_TYPE.KAFANA && SHOW_MENU_KAFANA ? <Menu goBack={() => exitMenu()} menu={MENU_TYPE.KAFANA} /> :
+                <div className="Invitation">
+                    {isAudioOn ?
+                        <button className='mute-btn' onClick={() => toggleAudio()}><MuteIcon /></button>
+                        : <button className='mute-btn' onClick={() => toggleAudio()}><PlayIcon /></button>}
 
-                <FadeIn transitionDuration={800} delay={500}>
-                    <div className='invitation-frame'>
-                        <div className='invitation-body'>
-                            <p>Welcome, dear <b className='accent'>{props.guest.displayName}</b>!</p>
-                            <p>
-                                Save the date:
-                            </p>
-                            <h3 className='accent'>
-                                25 November 2023
-                            </h3>
-                            <p>
-                                We are thrilled to invite you to our wedding celebration. This is the plan:
-                            </p>
-                            <div>
-                                {props.guest?.isKafana ? <p><b className='accent'>14:00 </b>Ruchek & matichno @<a href={'https://mk.wikipedia.org/wiki/%D0%9A%D0%B0%D1%84%D0%B5%D0%B0%D0%BD%D0%B0'}>kafana[TBA]</a></p> : null}
-                                {props.guest?.isZhurka ?
-                                    <p><b className='accent'>19:00 </b>Zhurka @<a href={'https://maps.app.goo.gl/2fh6s33rabbvTAsB6'}>Groovelyn</a>{SHOW_MENU && "*"} <a target="_blank" href={addToCalendarLink}><img className={'calendar-img'} width={'20em'} height={'20em'} src={require('./assets/calendar.png')} alt={'Add to Google Calendar'} /></a>
-                                    </p> : null}
+                    <FadeIn transitionDuration={800} delay={500}>
+                        <div className='invitation-frame'>
+                            <div className='invitation-body'>
+                                <p>Welcome, dear <b className='accent'>{props.guest.displayName}</b>!</p>
+                                <p>
+                                    Save the date:
+                                </p>
+                                <h3 className='accent'>
+                                    25 November 2023
+                                </h3>
+                                <p>
+                                    We are thrilled to invite you to our wedding celebration. This is the plan:
+                                </p>
+                                <div>
+                                    {props.guest?.isKafana ? <p><b className='accent'>14:30 </b>Ruchek & matichno @<a href={'https://maps.app.goo.gl/dsZ5KbYikZnW1Mvf8'}>Lira</a>{SHOW_MENU_KAFANA && "*"} </p> : null}
+                                    {props.guest?.isZhurka ?
+                                        <p><b className='accent'>19:00 </b>Zhurka @<a href={'https://maps.app.goo.gl/2fh6s33rabbvTAsB6'}>Groovelyn</a>{SHOW_MENU_ZHURKA && "*"} <a target="_blank" href={addToCalendarLink}><img className={'calendar-img'} width={'20em'} height={'20em'} src={require('./assets/calendar.png')} alt={'Add to Google Calendar'} /></a>
+                                        </p> : null}
+                                </div>
+                                <p>See you there!</p>
+                                <h3 className='accent'>♡</h3>
+                                <p>Marina & Nikola</p>
+                                {props.guest.isKafana && SHOW_MENU_KAFANA ? <p className='menu-link'>* <a onClick={() => setMenuView(MENU_TYPE.KAFANA)}>View drinks menu (Lira)</a></p>
+                                    : null}
+                                {props.guest.isZhurka && SHOW_MENU_ZHURKA ? <p className='menu-link'>* <a onClick={() => setMenuView(MENU_TYPE.ZHURKA)}>View drinks menu (Groovelyn)</a></p>
+                                    : null}
                             </div>
-                            <p>See you there!</p>
-                            <h3 className='accent'>♡</h3>
-                            <p>Marina & Nikola</p>
-                            {props.guest.isZhurka && SHOW_MENU ? <p className='menu-link'>* <a onClick={() => setMenuView(true)}>View drinks menu</a></p>
-                                : null}
                         </div>
-                    </div>
-                </FadeIn >
-            </div >
+                    </FadeIn >
+                </div >
     );
 }
 
--- src/Menu.tsx
+++ src/Menu.tsx
@@ -3,52 +3,92 @@ import FadeIn from 'react-fade-in';
 
 import './App.css';
 
-function Menu(props: { goBack: any }) {
-    return (
-        <div className="Invitation">
-            <FadeIn transitionDuration={800} delay={500}>
-                <div className='invitation-frame'>
-                    <div className='invitation-body'>
-                        <h3 className='accent'>
-                            Drinks Menu
-                        </h3>
-                        <div>
-                            <ul className='menu-items'>
-                                <li>Скопско</li>
-                                <li>Скопско Smooth</li>
-                                <li>Златен Даб</li>
-                                <li>Амстел</li>
-                                <li>Хајнекен безалкохолно</li>
-                                <li>Кромбахер пилс</li>
-                                <li>Кромбахер матно</li>
-                                <li>Кромбахер темно</li>
-                                <li>J&B</li>
-                                <li>Jameson</li>
-                                <li>Jim Beam</li>
-                                <li>Gordon's</li>
-                                <li>Smirnoff</li>
-                                <li>Finlandia</li>
-                                <li>Captain Morgan Gold</li>
-                                <li>Кампари-Тоник</li>
-                                <li>Џин-Тоник</li>
-                                <li>Бело вино (наливно)</li>
-                                <li>Црвено вино (наливно)</li>
-                                <li>Кока Кола</li>
-                                <li>Кока Кола Зеро</li>
-                                <li>Швепс</li>
-                                <li>Тоник</li>
-                                <li>Сок од портокал</li>
-                                <li>Сок од боровница</li>
-                                <li>Сок од јаболко</li>
-                                <li>Вода (кисела/обична)</li>
-                            </ul>
-                        </div>
-                        <p className='menu-link'>← <a onClick={() => props.goBack()}>Go back</a></p>
+export enum MENU_TYPE {
+    KAFANA = 'kafana',
+    ZHURKA = 'zhurka'
+}
 
+function Menu(props: { goBack: any, menu: string }) {
+    return (
+        props.menu == MENU_TYPE.ZHURKA ?
+            <div className="Invitation">
+                <FadeIn transitionDuration={800} delay={500}>
+                    <div className='invitation-frame'>
+                        <div className='invitation-body'>
+                            <h3 className='accent'>
+                                Drinks Menu - Groovelyn
+                            </h3>
+                            <div>
+                                <ul className='menu-items'>
+                                    <li>Скопско</li>
+                                    <li>Скопско Smooth</li>
+                                    <li>Златен Даб</li>
+                                    <li>Амстел</li>
+                                    <li>Хајнекен безалкохолно</li>
+                                    <li>Кромбахер пилс</li>
+                                    <li>Кромбахер матно</li>
+                                    <li>Кромбахер темно</li>
+                                    <li>J&B</li>
+                                    <li>Jameson</li>
+                                    <li>Jim Beam</li>
+                                    <li>Gordon's</li>
+                                    <li>Smirnoff</li>
+                                    <li>Finlandia</li>
+                                    <li>Captain Morgan Gold</li>
+                                    <li>Кампари-Тоник</li>
+                                    <li>Џин-Тоник</li>
+                                    <li>Бело вино (наливно)</li>
+                                    <li>Црвено вино (наливно)</li>
+                                    <li>Кока Кола</li>
+                                    <li>Кока Кола Зеро</li>
+                                    <li>Швепс</li>
+                                    <li>Тоник</li>
+                                    <li>Сок од портокал</li>
+                                    <li>Сок од боровница</li>
+                                    <li>Сок од јаболко</li>
+                                    <li>Вода (кисела/обична)</li>
+                                </ul>
+                            </div>
+                            <p className='menu-link'>← <a onClick={() => props.goBack()}>Go back</a></p>
+
+                        </div>
                     </div>
-                </div>
-            </FadeIn >
-        </div >
+                </FadeIn >
+            </div > : <div className="Invitation">
+                <FadeIn transitionDuration={800} delay={500}>
+                    <div className='invitation-frame'>
+                        <div className='invitation-body'>
+                            <h3 className='accent'>
+                                Drinks Menu - Lira
+                            </h3>
+                            <div>
+                                <ul className='menu-items'>
+                                    <li>Скопско</li>
+                                    <li>Скопско Smooth</li>
+                                    <li>Амстел</li>
+                                    <li>Тиквеш класик</li>
+                                    <li>Темјаника</li>
+                                    <li>Траминец</li>
+                                    <li>Т'га за југ</li>
+                                    <li>Сок - газиран</li>
+                                    <li>Сок - негазиран</li>
+                                    <li>Тиквеш лозова ракија</li>
+                                    <li>Узо</li>
+                                    <li>Мастика</li>
+                                    <li>Шток</li>
+                                    <li>Мартини</li>
+                                    <li>Кампари</li>
+                                    <li>Џин Gordon's</li>
+                                    <li>Вотка - Smirnoff</li>
+                                    <li>Вотка - Finlandia</li>
+                                </ul>
+                            </div>
+                            <p className='menu-link'>← <a onClick={() => props.goBack()}>Go back</a></p>
+
+                        </div>
+                    </div>
+                </FadeIn >
+            </div >
     );
 }
 
